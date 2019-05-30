a7f79d734d88585f587bdd94cc6b3624f8081932
Larvouu <ghiassiantoine@gmail.com>
2019-05-30 22:01:51 +0200

    merge Antoine - Inna - Sarah

--- JAVA/src/controller/Connexion.java
+++ JAVA/src/controller/Connexion.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testbdd;
-
 /**
  * @author Sarah
  */
+package controller;
 
-/*
- * Librairies importées
- */
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -19,16 +10,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
 /**
- * 
- * Connexion a votre BDD locale ou à  distance sur le serveur de l'ECE via le tunnel SSH
- * 
+ *
+ * Connexion a votre BDD locale ou à  distance sur le serveur de l'ECE via le
+ * tunnel SSH
+ *
  * @author segado
  */
-public class Connexion
-{
+public class Connexion {
 
     /**
      * Attributs prives : connexion JDBC, statement, ordre requete et resultat
@@ -60,19 +49,15 @@ public class Connexion
      * @throws java.sql.SQLException
      * @throws java.lang.ClassNotFoundException
      */
-    public Connexion(String nameDatabase, String loginDatabase, String passwordDatabase) throws SQLException, ClassNotFoundException 
-    {
+    public Connexion(String nameDatabase, String loginDatabase, String passwordDatabase) throws SQLException, ClassNotFoundException {
         // chargement driver "com.mysql.jdbc.Driver"
         Class.forName("com.mysql.jdbc.Driver");
-
-
 
         // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
         //pour Inna  jdbc:mysql://localhost:8889/
         String urlDatabase = "jdbc:mysql://localhost/" + nameDatabase;
 
-
-        //création d'une connexion JDBC à la base 
+        //création d'une connexion JDBC à la base
         conn = DriverManager.getConnection(urlDatabase, loginDatabase, passwordDatabase);
 
         // création d'un ordre SQL (statement)
@@ -82,6 +67,7 @@ public class Connexion
     /**
      * Constructeur avec 4 paramètres : username et password ECE, login et
      * password de la BDD à distance sur le serveur de l'ECE
+     *
      * @param usernameECE
      * @param passwordECE
      * @param loginDatabase
@@ -90,34 +76,32 @@ public class Connexion
      * @throws java.lang.ClassNotFoundException
      */
     /*public Connexion(String usernameECE, String passwordECE, String loginDatabase, String passwordDatabase) throws SQLException, ClassNotFoundException 
-    {
-        // chargement driver "com.mysql.jdbc.Driver"
-        Class.forName("com.mysql.jdbc.Driver");
+     {
+     // chargement driver "com.mysql.jdbc.Driver"
+     Class.forName("com.mysql.jdbc.Driver");
 
-        // Connexion via le tunnel SSH avec le username et le password ECE
-        SSHTunnel ssh = new SSHTunnel(usernameECE, passwordECE);
+     // Connexion via le tunnel SSH avec le username et le password ECE
+     SSHTunnel ssh = new SSHTunnel(usernameECE, passwordECE);
 
-        if (ssh.connect()) {
-            System.out.println("Connexion reussie");
+     if (ssh.connect()) {
+     System.out.println("Connexion reussie");
 
-            // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
-            String urlDatabase = "jdbc:mysql://localhost:3305/" + usernameECE;
+     // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
+     String urlDatabase = "jdbc:mysql://localhost:3305/" + usernameECE;
 
-            //crÃ©ation d'une connexion JDBC Ã  la base
-            conn = DriverManager.getConnection(urlDatabase, loginDatabase, passwordDatabase);
+     //crÃ©ation d'une connexion JDBC Ã  la base
+     conn = DriverManager.getConnection(urlDatabase, loginDatabase, passwordDatabase);
 
-            // crÃ©ation d'un ordre SQL (statement)
-            stmt = conn.createStatement();
-
-        }
-    }*/
-
+     // crÃ©ation d'un ordre SQL (statement)
+     stmt = conn.createStatement();
+     }
+     }*/
     /**
      * Méthode qui ajoute la table en parametre dans son ArrayList
+     *
      * @param table
      */
-    public void ajouterTable(String table) 
-    {
+    public void ajouterTable(String table) {
         tables.add(table);
     }
 
@@ -127,19 +111,16 @@ public class Connexion
      *
      * @param requete
      */
-    public void ajouterRequete(String requete) 
-    {
+    public void ajouterRequete(String requete) {
         requetes.add(requete);
     }
 
     /**
-     * Méthode qui ajoute la requete de MAJ en parametre dans son
-     * ArrayList
+     * Méthode qui ajoute la requete de MAJ en parametre dans son ArrayList
      *
      * @param requete
      */
-    public void ajouterRequeteMaj(String requete) 
-    {
+    public void ajouterRequeteMaj(String requete) {
         requetesMaj.add(requete);
     }
 
@@ -150,26 +131,19 @@ public class Connexion
      * @return
      * @throws java.sql.SQLException
      */
-
-
-    public ArrayList remplirChampsTable(ArrayList<String> table) throws SQLException
-    {
+    public ArrayList remplirChampsTable(ArrayList<String> table) throws SQLException {
         // récupération de l'ordre de la requete
         rset = stmt.executeQuery("SELECT * from " + table);
-
         // récupération du résultat de l'ordre
         rsetMeta = rset.getMetaData();
-
         // calcul du nombre de colonnes du resultat
         int nbColonne = rsetMeta.getColumnCount();
-
         // creation d'une ArrayList de String
         ArrayList<String> liste;
         liste = new ArrayList<>();
         String champs = "";
         // Ajouter tous les champs du resultat dans l'ArrayList
-        for (int i = 0; i < nbColonne; i++) 
-        {
+        for (int i = 0; i < nbColonne; i++) {
             champs = champs + " " + rsetMeta.getColumnLabel(i + 1);
         }
 
@@ -178,77 +152,64 @@ public class Connexion
 
         // ajouter les champs de la ligne dans l'ArrayList
         liste.add(champs);
-
         // Retourner l'ArrayList
         return liste;
     }
 
     /**
      * Methode qui retourne l'ArrayList des champs de la REQUETE en parametre
+     *
      * @param requete
-     * @return 
+     * @return
      * @throws java.sql.SQLException
      */
     public ArrayList remplirChampsRequete(String requete) throws SQLException {
         // récupération de l'ordre de la requete
         rset = stmt.executeQuery(requete);
-
         // récupération du résultat de l'ordre
         rsetMeta = rset.getMetaData();
-
         // calcul du nombre de colonnes du resultat
         int nbColonne = rsetMeta.getColumnCount();
-
         // creation d'une ArrayList de String
         ArrayList<String> liste;
         liste = new ArrayList<String>();
-
-        // tant qu'il reste une ligne 
-        while (rset.next()) 
-        {
+        // tant qu'il reste une ligne
+        while (rset.next()) {
             String champs;
             champs = rset.getString(1); // ajouter premier champ
-
             // Concatener les champs de la ligne separes par ,
-            for (int i = 1; i < nbColonne; i++) 
-            {
+            for (int i = 1; i < nbColonne; i++) {
                 champs = champs + "," + rset.getString(i + 1);
             }
 
             // ajouter un "\n" à  la ligne des champs
             champs = champs + "\n";
-
             // ajouter les champs de la ligne dans l'ArrayList
             liste.add(champs);
         }
-
         // Retourner l'ArrayList
         return liste;
     }
 
     /**
      * Méthode qui execute une requete de MAJ en parametre
+     *
      * @param requeteMaj
      * @throws java.sql.SQLException
      */
-    public void executeUpdate(String requeteMaj) throws SQLException 
-    {
+    public void executeUpdate(String requeteMaj) throws SQLException {
         stmt.executeUpdate(requeteMaj);
     }
 
-    public Statement getStmt()
-    {
+    public Statement getStmt() {
         return stmt;
     }
 
     public ResultSetMetaData getRsetMeta() {
         return rsetMeta;
     }
+
     public void setRsetMeta(ResultSetMetaData rsetmeta) {
         this.rsetMeta = rsetmeta;
-
     }
-
 }
-
-
--- JAVA/src/controller/Main.java
+++ JAVA/src/controller/Main.java
@@ -5,16 +5,161 @@
  */
 package controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author ghias
  */
 public class Main {
-    
+
     //La méthode main
     public static void main(String[] args) {
-        
-        
+
+        boolean quit = false;
+        do {
+            try {
+                //Connexion à la base de données
+                Connexion maConnexion = new Connexion("ecole", "root", ""); // Ici il faut enlever le « root » pour le mdp
+                System.out.println("Connexion à la base de données successfull");
+                System.out.println("Que souhaitez vous faire ?");
+                System.out.println("1. Afficher les tables");
+                System.out.println("2. Insérer des données");
+                System.out.println("3. Modifier les données");
+                Scanner sc = new Scanner(System.in);
+                int choix_menu = 0;
+                choix_menu = sc.nextInt();
+                switch (choix_menu) {
+                    case 1:
+                        //REQUETE SELECT
+                        ResultSet résultats = null;
+                        String requete = null;
+                        System.out.println("Quel table voulez vous afficher ?");
+                        System.out.println("1. Personne");
+                        System.out.println("2. Eleve");
+                        System.out.println("3. blabla");
+                        int choix_table = sc.nextInt();
+                        try {
+                            switch (choix_table) {
+                                case 1:
+                                    requete = "SELECT * FROM personne";
+                                    break;
+                                case 2:
+                                    requete = "SELECT * FROM personne";
+                                    break;
+                                case 3:
+                                    requete = "SELECT * FROM personne";
+                                    break;
+                            }
+                            ResultSet rs = maConnexion.getStmt().executeQuery(requete);
+                            while (rs.next()) {
+                                String type = rs.getString("type");
+                                System.out.println("type = " + type);
+                                String prenom = rs.getString("prenom");
+                                System.out.println("prenom = " + prenom);
+                                String nom = rs.getString("nom");
+                                System.out.println("nom = " + nom);
+                            }
+                        } catch (SQLException e) {
+                            //traitement de l'exception
+                            System.out.println("exectepeiton SQL" + e);
+                        }
+                        break;
+                    ///FIN AFFICHER TABLE
+                    case 2:
+                        //DEBUT INSERER DONNEES
+                        System.out.println("Dans quelle table voulezvous inserer des données ?");
+                        System.out.println("1. Personne");
+                        System.out.println("2. Eleve");
+                        System.out.println("3. blabla");
+                        String table_insert = null;
+                        String nom_personne = null;
+                        String prenom_personne = null;
+                        String type_personne = null;
+                        int choix_table_donnees = sc.nextInt();
+                        switch (choix_table_donnees) {
+                            case 1:
+                                table_insert = "personne";
+                                System.out.println("Veuillez rentrer le nouveau nom");
+                                nom_personne = sc.next();
+                                System.out.println("Veuillez rentrer le nouveau prenom");
+                                prenom_personne = sc.next();
+                                System.out.println("Veuillez rentrer le nouveau type");
+                                type_personne = sc.next();
+                                maConnexion.getStmt().executeUpdate("INSERT into personne (nom, prenom, type) VALUES (' " + nom_personne + " ',' " + prenom_personne + " ',' " + type_personne + " ')");
+                                break;
+                            case 2:
+                                table_insert = "eleve";
+                                break;
+                            case 3:
+                                table_insert = "blabla";
+                                break;
+                        }
+                        break;
+                    //FIN INSERER DONNEES
+                    case 3:
+                        //DEBUT MODIF DONNEES
+                        System.out.println("Quelle table souhaitez vous update");
+                        System.out.println("1. Personne");
+                        System.out.println("2. Eleve");
+                        System.out.println("3. blabla");
+                        String table_update = null;
+                        String nom_personne_update = null;
+                        String prenom_personne_update = null;
+                        String type_personne_update = null;
+                        String amodifier = null;
+                        int champ_choix = 0;
+
+                        switch (champ_choix) {
+                            case 1:
+                                System.out.println("Quelle champ souhaitez vous modifier ?");
+                                System.out.println("1. nom");
+                                System.out.println("2. prenom");
+                                System.out.println("3. type");
+                                int choix_col_update = 0;
+                                choix_col_update = sc.nextInt();
+                                switch (choix_col_update) {
+                                    case 1:
+                                        System.out.println("Rentre le prenom de la personne a modifier");
+                                        amodifier = sc.next();
+                                        System.out.println("Veuillez rentrer le nouveau nom");
+                                        nom_personne_update = sc.next();
+
+                                        maConnexion.getStmt().executeUpdate(" UPDATE personne set nom='" + nom_personne_update + "' where prenom = '" + amodifier + "' ");
+                                        break;
+                                    case 2:
+                                        System.out.println("Rentre le nom de la personne a modifier");
+                                        amodifier = sc.next();
+                                        System.out.println("Veuillez rentrer le nouveau prenom");
+                                        prenom_personne_update = sc.next();
+
+                                        maConnexion.getStmt().executeUpdate(" UPDATE personne set prenom='" + prenom_personne_update + "' where nom = '" + amodifier + "' ");
+                                        break;
+                                    case 3:
+                                        System.out.println("Rentre le nom de la personne a modifier");
+                                        amodifier = sc.next();
+                                        System.out.println("Veuillezrentrer le nouveau type");
+                                        type_personne_update = sc.next();
+
+                                        maConnexion.getStmt().executeUpdate(" UPDATE personne set  type='" + type_personne_update + "' where nom = '" + amodifier + "' ");
+                                        break;
+                                }
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                        }
+                        break;
+                }
+            } catch (SQLException s) {
+                System.out.println(s + " SQL excep");
+            } catch (ClassNotFoundException e) {
+                System.out.println(e + "Class Not Found !");
+            }
+        } while (!quit);
+
     }//fin main
-    
+
 }
