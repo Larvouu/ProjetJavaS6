2a5353fcacafa4c2b5a6963111fc594251f7cce2
Schouketta <sarah.le@live.fr>
2019-06-07 21:57:09 +0200

    test

--- JAVA/src/vue/JpanelAdmin.java
+++ JAVA/src/vue/JpanelAdmin.java
@@ -33,6 +33,7 @@ public class JpanelAdmin extends javax.swing.JPanel {
         jButtonRechercherProf = new javax.swing.JButton();
         jButtonRechercherClasse = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jTabbedPane2 = new javax.swing.JTabbedPane();
         jPanel3 = new javax.swing.JPanel();
@@ -87,22 +88,29 @@ public class JpanelAdmin extends javax.swing.JPanel {
         jLabel1.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
         jLabel1.setText("Bonjour Admin, que voulez-vous rechercher ?");
 
+        jButton1.setText("Vincent c'est le meilleur");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(65, 65, 65)
-                .addComponent(jButtonRechercherEleve)
-                .addGap(40, 40, 40)
-                .addComponent(jButtonRechercherProf)
-                .addGap(40, 40, 40)
-                .addComponent(jButtonRechercherClasse)
-                .addContainerGap(64, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(247, 247, 247))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(65, 65, 65)
+                        .addComponent(jButtonRechercherEleve)
+                        .addGap(40, 40, 40)
+                        .addComponent(jButtonRechercherProf)
+                        .addGap(40, 40, 40)
+                        .addComponent(jButtonRechercherClasse))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(357, 357, 357)
+                        .addComponent(jButton1)))
+                .addContainerGap(64, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,7 +122,9 @@ public class JpanelAdmin extends javax.swing.JPanel {
                     .addComponent(jButtonRechercherEleve, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonRechercherProf, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonRechercherClasse, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(214, Short.MAX_VALUE))
+                .addGap(66, 66, 66)
+                .addComponent(jButton1)
+                .addContainerGap(123, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Rechercher", jPanel1);
@@ -448,6 +458,7 @@ public class JpanelAdmin extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonChangerEleveClasse;
     private javax.swing.JButton jButtonModifAppreciationDiscipline;
     private javax.swing.JButton jButtonModifBulletin;
