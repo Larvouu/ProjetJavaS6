f84411c395f0d0fbbee5d73feaef01df1b1b9dcf
Larvouu <ghiassiantoine@gmail.com>
2019-06-10 01:27:51 +0200

    javadoc des packages

---
+++ JAVA/src/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Le package controller contient les classesDAO et la classe Main. Il regroupe les classes qui effectuent un traitement (ou un controle) sur les données des classes que l'on retrouve dans le package Modèle.
+ */
+package controller;
